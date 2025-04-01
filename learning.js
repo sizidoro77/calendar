b198baea1591e13658485859dae14ee5822c63fd
Samuel Izidoro <s.izidoro77@gmail.com>
2025-04-01 16:25:22 -0300

    comit
    1 parte do calendar feita.

---
+++ learning.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function () 
+    const monthYer = document.getElementById('month-year'))
+
+    const months = [
+        'January', 'february', 'March', 'April,''
+    ]
