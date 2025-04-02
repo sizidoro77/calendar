9b50657301296db5f5afd93bca016da7e64453ec
Samuel Izidoro <s.izidoro77@gmail.com>
2025-04-02 18:21:54 -0300

    comit
    Primeira parte do java script feita.

--- learning.js
+++ learning.js
@@ -1,6 +1,32 @@
-document.addEventListener('DOMContentLoaded', function () 
-    const monthYer = document.getElementById('month-year'))
+document.addEventListener('DOMContentLoaded', function () {
+    const monthYear = document.getElementById('month-year');
+    const daysContainer = document.getElementById('days');
 
     const months = [
-        'January', 'february', 'March', 'April,''
-    ]
+        'January', 'February', 'March', 'April', 'May', 'June',
+        'July', 'August', 'September', 'October', 'November', 'December'
+    ];
+
+    let currentDate = new Date();
+    let today = new Date();
+
+    function renderCalendar(date) {
+        const year = date.getFullYear();
+        const month = date.getMonth();
+        const firstDay = new Date(year, month, 1).getDay();
+        const lastDay = new Date(year, month + 1, 0).getDate();
+
+        monthYear.textContent = `${months[month]} ${year}`;
+
+        daysContainer.innerHTML = '';
+
+        // Current month's dates 
+        for (let i = 1; i <= lastDay; i++) {
+            const dayDiv = document.createElement('div');
+            dayDiv.textContent = i
+            if (i === today.getDate() && month === today.getMonth() && year === today.getFullYear()) {
+        }
+    }
+
+    renderCalendar(currentDate);
+});
